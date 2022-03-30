40d1c06107294d60f99da3f342437029ae2af913
Leo Hilsheimer <leo.hilsheimer@googlemail.com>
2022-03-30 23:08:27 +0200

    colors and harmony conf

---
+++ js/visualizer/conf/harmony.js
@@ -0,0 +1,47 @@
+let harmony_array = {
+    // MODE_FREE
+    0: {
+        0: CONSONANT,
+        1: DISSONANT,
+        2: DISSONANT,
+        3: SEMISONANT,
+        4: SEMISONANT,
+        5: CONSONANT,
+        6: DISSONANT,
+        7: CONSONANT,
+        8: SEMISONANT,
+        9: SEMISONANT,
+        10: DISSONANT,
+        11: DISSONANT,
+    },
+    // MODE_MAJOR
+    1: {
+        0: CONSONANT,
+        1: DISSONANT,
+        2: SEMISONANT,
+        3: DISSONANT,
+        4: CONSONANT,
+        5: CONSONANT,
+        6: DISSONANT,
+        7: CONSONANT,
+        8: DISSONANT,
+        9: SEMISONANT,
+        10: DISSONANT,
+        11: SEMISONANT,
+    },
+    // MODE_MINOR
+    0: {
+        0: CONSONANT,
+        1: DISSONANT,
+        2: SEMISONANT,
+        3: CONSONANT,
+        4: DISSONANT,
+        5: CONSONANT,
+        6: DISSONANT,
+        7: CONSONANT,
+        8: SEMISONANT,
+        9: DISSONANT,
+        10: SEMISONANT,
+        11: DISSONANT,
+    }
+}
