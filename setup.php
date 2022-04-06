33a54255311fd6e07c95b722b7cfe234778d0205
kathi ruell <k.ruell@gmx.net>
2022-04-06 15:34:20 +0200

    new main layout

---
+++ setup.php
@@ -0,0 +1,44 @@
+<div class="page" id="setup-page">
+    <a href="#about-page" class="blur-about">ABOUT</a>
+    <a href="visualizer.html" class="blur-visualiser">VISUALISER</a>
+
+    <div class="setup-complete">
+    <div class="setup-sentence">VISUALIZE YOUR MUSIC LIKE</div>
+
+    <div class="setup-categories center">
+        <button data-pref-preview class="setup-key" data-key="animation_style" data-format="key,"></button
+        ><br />
+        <button data-pref-preview class="setup-key" data-key="shape_style" data-format="key,"></button
+        ><br />
+        <button data-pref-preview class="setup-key" data-key="color_style" data-format="&key"></button>
+    </div>
+    <div class="setup-popup" data-key="shape_style" hidden="true">
+        <div class="center">
+        <button data-pref-set data-key="shape_style" data-value="0" data-format="key,"></button><br />
+        <button data-pref-set data-key="shape_style" data-value="1" data-format="key,"></button><br />
+        <button data-pref-set data-key="shape_style" data-value="2" data-format="or key"></button><br />
+        </div>
+    </div>
+    <div class="setup-popup" data-key="animation_style" hidden="true">
+        <div class="center">
+        <button data-pref-set data-key="animation_style" data-value="0" data-format="key,"></button><br />
+        <button data-pref-set data-key="animation_style" data-value="1" data-format="key,"></button><br />
+        <button data-pref-set data-key="animation_style" data-value="2" data-format="or key"></button><br />
+        </div>
+    </div>
+    <div class="setup-popup" data-key="color_style" hidden="true">
+        <div class="center">
+        <button data-pref-set data-key="color_style" data-value="0" data-format="key,"></button><br />
+        <button data-pref-set data-key="color_style" data-value="1" data-format="key,"></button><br />
+        <button data-pref-set data-key="color_style" data-value="2" data-format="or key"></button><br />
+        </div>
+    </div>
+
+    <div class="vignette-setup">
+        <button class="square" data-vignette-selector="v1"></button>
+        <button class="round-edge" data-vignette-selector="v2"></button>
+        <button class="circle" data-vignette-selector="v3"></button>
+    </div>
+
+    </div>
+</div>
