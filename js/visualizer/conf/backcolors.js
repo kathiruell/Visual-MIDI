bbe6545b75e3616f7b3cb20b6edd2c558e25e992
kathi ruell <k.ruell@gmx.net>
2022-04-07 11:54:26 +0200

    backcolors

---
+++ js/visualizer/conf/backcolors.js
@@ -0,0 +1,71 @@
+let backcolors_array = {
+  // intimate
+  0: {
+    // MAJOR
+    0: [
+      [237, 193, 198], //unten
+      [255, 255, 129], //oben
+    ],
+    // minor
+    1: [
+      [195, 155, 167], //unten
+      [165, 210, 158], //oben
+    ],
+    // Augmented
+    2: [
+      [241, 158, 156], //unten
+      [235, 220, 161], //oben
+    ],
+    // Diminished
+    2: [
+      [235, 220, 161], //unten
+      [186, 230, 172], //oben
+    ],
+  },
+  // luminous
+  1: {
+    // MAJOR
+    0: [
+      [101, 255, 25], //unten
+      [255, 153, 29], //oben
+    ],
+    // minor
+    1: [
+      [0, 0, 138], //unten
+      [0, 147, 0], //oben
+    ],
+    // Augmented
+    2: [
+      [0, 214, 0], //unten
+      [255, 76, 84], //oben
+    ],
+    // Diminished
+    2: [
+      [255, 72, 83], //unten
+      [0, 60, 255], //oben
+    ],
+  },
+  // gloom
+  2: {
+    // MAJOR
+    0: [
+      [59, 216, 169], //unten
+      [255, 255, 0], //oben
+    ],
+    // minor
+    1: [
+      [59, 24, 63], //unten
+      [0, 87, 73], //oben
+    ],
+    // Augmented
+    2: [
+      [123, 0, 151], //unten
+      [221, 156, 0], //oben
+    ],
+    // Diminished
+    2: [
+      [195, 155, 167], //unten
+      [237, 193, 198], //oben
+    ],
+  },
+};
