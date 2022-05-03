2dbbb30f1d1df7fdd7ef46dd31f4b465492cd785
Leo Hilsheimer <leo.hilsheimer@googlemail.com>
2022-05-03 22:34:21 +0200

    refined text rotation

--- js/rotate.js
+++ js/rotate.js
@@ -1,21 +1,25 @@
 $(function() {
     $('[data-rotate]').each(function() {
-        let rotate_speed = $(this).attr('data-rotate-speed') || 500
-        let fade_speed = $(this).attr('data-fade-speed') || 250
+        let rotate_speed = $(this).attr('data-rotate-speed') ?? 4000
+        let fade_speed = parseInt( $(this).attr('data-fade-speed') ?? 250 )
+        let fixed_height = $(this).is('[data-fixed-height]')
         let $first = $(this).find('[data-rotate-item]').first()
         $first.attr('data-active', '1')
 
-
         let $container = $(this)
         let $display = $("<div data-rotate-display></div>")
-        let $new = $("<div></div>").html($first.contents().clone())
+        // let $new = $("<div></div>").html($first.contents().clone())
+        let $new = rotateGetContents($first)
         $container.append($display)
         $container.css('position', 'relative')
         $display.append($new)
 
-        let debug = 0
+        $container.rotateInitHeight()
+
         window.setInterval(function() {
-            // if (debug++ > 2) return
+            
+            if (!$container.isInViewport()) return
+
             $container.find('[data-rotate-item][data-active="1"]').each(function() {
                 $(this).attr('data-active', '0')
                 let $next = $(this).next('[data-rotate-item]')
@@ -29,8 +33,8 @@ $(function() {
                 $old.css('opacity', 1)
 
                 // add new contents
-                $new = $("<div></div>").html($next.contents().clone())
-                $old.css('opacity', 0)
+                $new = rotateGetContents($next)
+                $new.css('opacity', 0)
                 $display.append($new)
                 $next.attr('data-active', '1')
 
@@ -51,7 +55,9 @@ $(function() {
                 })
 
                 // animate height 
-                $display.height($old.height()).animate({ height: new_height }, fade_speed)
+                if (!fixed_height) {
+                    $display.height($old.height()).animate({ height: new_height }, fade_speed)
+                }
             });
 
         }, rotate_speed);
@@ -59,3 +65,32 @@ $(function() {
     });
 
 });
+
+$.fn.rotateInitHeight = function() {
+    let fixed_height = $(this).is('[data-fixed-height]')
+    if (!fixed_height) return
+
+    // secretly render all items and find max height
+    let $display = $(this).find('[data-rotate-display]')
+    let max_height = 0
+    $(this).find('[data-rotate-item]').each(function() {
+        $item = rotateGetContents($(this))
+        $item.css('opacity', 0)
+        $display.append($item)
+        if ($item.height() > max_height) max_height = $item.height()
+        $item.remove()
+    });
+    $display.height(max_height)
+};
+
+function rotateGetContents($template) {
+    return $('<div></div>').html($template.contents().clone())
+}
+
+$.fn.isInViewport = function() {
+    var elementTop = $(this).offset().top;
+    var elementBottom = elementTop + $(this).outerHeight();
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+};
